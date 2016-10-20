8bd3d64541e68414e4f8b8f8f3076ea7546f568d
Ziad Saab <ziad.saab@gmail.com>
2016-10-20 10:42:57 -0400

    removed extra div in tuition dates to match style guide markup

--- src/components/tables/TuitionDates.js
+++ src/components/tables/TuitionDates.js
@@ -13,28 +13,26 @@ const TuitionDates = React.createClass({
     },
     render() {
         return (
-            <div>
-                <div className={this.props.price ? 'row' : 'row full'}>
-                    <div className="cell">
-                        {this.props.startDate.replace(/[a-z]+$/i, '')}<sup>{this.props.startDate.match(/\d+(.*)/)[1]}</sup>
-                        &nbsp;- &nbsp;
-                        {this.props.endDate.replace(/[a-z]+$/i, '')}<sup>{this.props.endDate.match(/\d+(.*)/)[1]}</sup>
-                    </div>
-                    <div className="cell">{this.props.days}</div>
-                    <div className="cell">{this.props.hours}</div>
-                    <div className="cell">
-                        {this.props.price
-                            ? this.props.earlyBird
-                            ? this._formatPrice(this.props.price - 500)
-                            : this._formatPrice(this.props.price)
-                            : 'Full'}
-                        {this.props.earlyBird
-                            ? <span>
-                                <span className="fa fa-bolt" aria-hidden="true"/>
-                                <span className="sr-only">Early bird price</span>
-                            </span>
-                            : null}
-                    </div>
+            <div className={this.props.price ? 'row' : 'row full'}>
+                <div className="cell">
+                    {this.props.startDate.replace(/[a-z]+$/i, '')}<sup>{this.props.startDate.match(/\d+(.*)/)[1]}</sup>
+                    &nbsp;- &nbsp;
+                    {this.props.endDate.replace(/[a-z]+$/i, '')}<sup>{this.props.endDate.match(/\d+(.*)/)[1]}</sup>
+                </div>
+                <div className="cell">{this.props.days}</div>
+                <div className="cell">{this.props.hours}</div>
+                <div className="cell">
+                    {this.props.price
+                        ? this.props.earlyBird
+                        ? this._formatPrice(this.props.price - 500)
+                        : this._formatPrice(this.props.price)
+                        : 'Full'}
+                    {this.props.earlyBird
+                        ? <span>
+                            <span className="fa fa-bolt" aria-hidden="true"/>
+                            <span className="sr-only">Early bird price</span>
+                        </span>
+                        : null}
                 </div>
             </div>
 
