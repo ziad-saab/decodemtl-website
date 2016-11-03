a3011c1924013ea08f1bcd9b8776f30d048af95e
Kevin Khoury <kev_khoury@me.com>
2016-11-03 17:38:47 -0400

    add url to event schema

--- src/components/modules/UpcomingEvents.js
+++ src/components/modules/UpcomingEvents.js
@@ -16,6 +16,7 @@ const eventsListJson = JSON.stringify(
                     "endDate": "2016-11-25T16:00",
                     "description": "Looking for smart, hungry web developers to join your team? Come meet the graduates from our Web Development Immersive program!",
                     "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F24308952%2F137894962856%2F1%2Foriginal.jpg?w=1000&rect=0%2C128%2C1024%2C512&s=2da049f01bafa7cee33575234dd4f67a",
+                    "url": "https://www.eventbrite.com/e/demo-day-web-developers-for-hire-tickets-27852866674",
                     "location": {
                         "@type": "Place",
                         "name": "WeWork Place Ville Marie",
