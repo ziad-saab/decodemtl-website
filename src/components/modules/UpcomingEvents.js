91c3ca22780f517974d5f464fb01b57d7b6af694
Kevin Khoury <kev_khoury@me.com>
2016-11-03 17:36:27 -0400

    update event schema to list

--- src/components/modules/UpcomingEvents.js
+++ src/components/modules/UpcomingEvents.js
@@ -4,21 +4,29 @@ import {Link} from 'react-router';
 const eventsListJson = JSON.stringify(
     {
         "@context": "http://schema.org",
-        "@type": "BusinessEvent",
-        "name": "DecodeMTL Demo Day - Web Developers for Hire",
-        "startDate": "2016-11-25T13:00",
-        "endDate": "2016-11-25T16:00",
-        "description": "Looking for smart, hungry web developers to join your team? Come meet the graduates from our Web Development Immersive program!",
-        "image":"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F24308952%2F137894962856%2F1%2Foriginal.jpg?w=1000&rect=0%2C128%2C1024%2C512&s=2da049f01bafa7cee33575234dd4f67a",
-        "location": {
-            "@type": "Place",
-            "name": "WeWork Place Ville Marie",
-            "address": "4-3 Place Ville Marie, Montreal QC H3B 2E3"
-        },
-        "offers": {
-            "@type": "Offer",
-            "url": "https://www.eventbrite.com/e/demo-day-web-developers-for-hire-tickets-27852866674"
-        }
+        "@type": "ItemList",
+        "itemListElement": [
+            {
+                "@type": "ListItem",
+                "position": 1,
+                "item": {
+                    "@type": "BusinessEvent",
+                    "name": "DecodeMTL Demo Day - Web Developers for Hire",
+                    "startDate": "2016-11-25T13:00",
+                    "endDate": "2016-11-25T16:00",
+                    "description": "Looking for smart, hungry web developers to join your team? Come meet the graduates from our Web Development Immersive program!",
+                    "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F24308952%2F137894962856%2F1%2Foriginal.jpg?w=1000&rect=0%2C128%2C1024%2C512&s=2da049f01bafa7cee33575234dd4f67a",
+                    "location": {
+                        "@type": "Place",
+                        "name": "WeWork Place Ville Marie",
+                        "address": "4-3 Place Ville Marie, Montreal QC H3B 2E3"
+                    },
+                    "offers": {
+                        "@type": "Offer",
+                        "url": "https://www.eventbrite.com/e/demo-day-web-developers-for-hire-tickets-27852866674"
+                    }
+                }
+            }]
     });
 
 const UpcomingEvents = (props) => {
