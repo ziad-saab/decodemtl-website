34591b9200e469ab7e2e122656180e8105b5e32e
Kevin Khoury <kev_khoury@me.com>
2016-11-03 16:45:27 -0400

    added events schema markup

--- src/components/modules/UpcomingEvents.js
+++ src/components/modules/UpcomingEvents.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+const eventsJson = JSON.stringify({
+    "@context": "http://schema.org",
+    "@type": "BusinessEvent",
+    "organizer": "DecodeMTL",
+    "url": "http://www.decodemtl.com",
+    "event": "http://www.eventbrite.com/o/decodemtl-8462090876"
+});
+
 const UpcomingEvents = (props) => {
     const filterDates = ({startDate}) => {
         return props.filterOld ? new Date(startDate).getTime() >= new Date().getTime() : startDate;
@@ -15,11 +23,11 @@ const UpcomingEvents = (props) => {
                 <p>{props.text}</p>
                 <ul className="events-list">
                     {props.events.filter(filterDates).slice(0, props.limit ? props.limit : props.events.length).map((event, i) => (
-                        <li key={i} className="text-body-small" itemScope itemType="http://schema.org/Event">
-                            <a className="event-url" href={event.url} itemProp="url" target="_blank">
-                                <div className="event-date" itemProp="startDate">{event.startDate}</div>
-                                <div className="event-name" itemProp="name">{event.eventName}</div>
-                                <div className="event-description" itemProp="description">{event.description}
+                        <li key={i} className="text-body-small">
+                            <a className="event-url" href={event.url} target="_blank">
+                                <div className="event-date">{event.startDate}</div>
+                                <div className="event-name">{event.eventName}</div>
+                                <div className="event-description">{event.description}
                                     <span className="event-arrow fa fa-chevron-circle-right text-body-large"
                                           aria-hidden="true"/>
                                     <span className="visually-hidden">Event URL</span>
@@ -36,6 +44,7 @@ const UpcomingEvents = (props) => {
                     </Link>
                         : null}
                 </div>
+                <script type="application/ld+json" dangerouslySetInnerHTML={{__html: eventsJson}}/>
             </div>
             {/*<!-- /.wrapper -->*/}
         </section>
