01de96e4a24b043172217524edca02231f4d0c05
Kevin Khoury <kev_khoury@me.com>
2016-11-03 17:40:59 -0400

    removed events rich snippet

--- src/components/modules/UpcomingEvents.js
+++ src/components/modules/UpcomingEvents.js
@@ -1,35 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-const eventsListJson = JSON.stringify(
-    {
-        "@context": "http://schema.org",
-        "@type": "ItemList",
-        "itemListElement": [
-            {
-                "@type": "ListItem",
-                "position": 1,
-                "item": {
-                    "@type": "BusinessEvent",
-                    "name": "DecodeMTL Demo Day - Web Developers for Hire",
-                    "startDate": "2016-11-25T13:00",
-                    "endDate": "2016-11-25T16:00",
-                    "description": "Looking for smart, hungry web developers to join your team? Come meet the graduates from our Web Development Immersive program!",
-                    "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F24308952%2F137894962856%2F1%2Foriginal.jpg?w=1000&rect=0%2C128%2C1024%2C512&s=2da049f01bafa7cee33575234dd4f67a",
-                    "url": "https://www.eventbrite.com/e/demo-day-web-developers-for-hire-tickets-27852866674",
-                    "location": {
-                        "@type": "Place",
-                        "name": "WeWork Place Ville Marie",
-                        "address": "4-3 Place Ville Marie, Montreal QC H3B 2E3"
-                    },
-                    "offers": {
-                        "@type": "Offer",
-                        "url": "https://www.eventbrite.com/e/demo-day-web-developers-for-hire-tickets-27852866674"
-                    }
-                }
-            }]
-    });
-
 const UpcomingEvents = (props) => {
     const filterDates = ({startDate}) => {
         return props.filterOld ? new Date(startDate).getTime() >= new Date().getTime() : startDate;
@@ -65,7 +36,6 @@ const UpcomingEvents = (props) => {
                     </Link>
                         : null}
                 </div>
-                <script type="application/ld+json" dangerouslySetInnerHTML={{__html: eventsListJson}}/>
             </div>
             {/*<!-- /.wrapper -->*/}
         </section>
