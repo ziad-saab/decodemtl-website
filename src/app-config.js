af7a557dacb23bbd88dfe8683eed9f1a0f963032
Kevin Khoury <kev_khoury@me.com>
2016-11-23 16:18:05 -0500

    remove commented code in app config - now located in react helmet per page

--- src/app-config.js
+++ src/app-config.js
@@ -7,12 +7,6 @@ module.exports = {
         titleTemplate: '%s | DecodeMTL',
         meta: [
           {charset: 'utf-8'},
-          // {name: 'description', content: ''},
-          // {property: 'og:url', content: 'http://www.decodemtl.com'},
-          // {property: 'og:type', content: 'website'},
-          // {property: 'og:title', content: 'Learn to Code at DecodeMTL'},
-          // {property: 'og:description', content: 'DecodeMTL offer\'s Montreal\'s most dynamic courses in tech. Learn to code through project based teaching from industry experts in a stimulating environment.'},
-          // {property: 'og:image', content: 'https://www.decodemtl.com/assets/images/video-screenshot.jpg'},
         ]
       },
       fr: {
@@ -20,12 +14,6 @@ module.exports = {
         titleTemplate: '%s | DecodeMTL',
         meta: [
           {charset: 'utf-8'},
-          // {name: 'description', content: 'meta description'},
-          // {property: 'og:url', content: 'http://www.decodemtl.com'},
-          // {property: 'og:type', content: 'website'},
-          // {property: 'og:title', content: 'Learn to Code at DecodeMTL'},
-          // {property: 'og:description', content: 'DecodeMTL offer\'s Montreal\'s most dynamic courses in tech. Learn to code through project based teaching from industry experts in a stimulating environment.'},
-          // {property: 'og:image', content: 'https://www.decodemtl.com/assets/images/video-screenshot.jpg'},
         ]
       }
     }
